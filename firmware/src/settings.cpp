03b951562326695509389662abbd8c1f2331bfc3
axlan <feros32@gmail.com>
2021-12-27 18:40:09 -0800

    Added scrolling to settings.

--- firmware/src/settings.cpp
+++ firmware/src/settings.cpp
@@ -2,25 +2,58 @@
 
 #include <stdlib.h>
 
-#define SCREEN_WIDTH 128  // OLED display width, in pixels
-#define SCREEN_HEIGHT 64  // OLED display height, in pixels
-#define YELLOW_HEIGHT 16  // OLED yellow portion height, in pixels
+#include "icons.h"
+
+#define ARROW_WIDTH 8
+#define ARROW_HEIGHT 4
+
+void Settings::Up() { scroll_ = (scroll_ > 0) ? scroll_ - 1 : scroll_; }
+
+void Settings::Down() {
+  scroll_ = (scroll_ < wm_helper_.GetNumSettings() + NUM_SPECIAL_SETTINGS - 1)
+                ? scroll_ + 1
+                : scroll_;
+}
 
 void Settings::Display() {
   display_.clearDisplay();
 
-  display_.setTextSize(1);               // Normal 1:1 pixel scale
-  display_.setTextColor(SSD1306_WHITE);  // Draw white text
-  display_.setCursor(0, 0);           
+  display_.setTextSize(1);              // Normal 1:1 pixel scale
+  display_.setTextColor(SSD1306_WHITE); // Draw white text
+  display_.setCursor(0, 0);
 
-  display_.println(String("IP: ") + WiFi.localIP().toString());
+  if (scroll_ < 1) {
+    display_.println(
+        F("To configure WiFi, connect a phone to the SSID \"LifeTrackerAP\". "
+          "Otherwise, point your browser at this IP."));
+  }
+  if (scroll_ < 2) {
+    display_.println(String("IP: ") + WiFi.localIP().toString());
+  }
 
-  for (size_t i = 0; i < wm_helper_.GetNumSettings(); i++) {
-    auto& setting = wm_helper_.GetSettingParam(i);
+  size_t i =
+      (scroll_ > NUM_SPECIAL_SETTINGS) ? scroll_ - NUM_SPECIAL_SETTINGS : 0;
+  for (; i < wm_helper_.GetNumSettings(); i++) {
+    auto &setting = wm_helper_.GetSettingParam(i);
     display_.print(setting.getLabel());
     display_.print(F(": "));
     display_.println(setting.getValue());
   }
-  
+
+  display_.fillRect(display_.width() - ARROW_WIDTH,
+                    display_.height() - ARROW_HEIGHT * 2 - 2, ARROW_WIDTH,
+                    ARROW_HEIGHT * 2 + 2, 0);
+
+  if (scroll_ > 0) {
+    display_.drawBitmap(display_.width() - ARROW_WIDTH,
+                        display_.height() - ARROW_HEIGHT * 2 - 1, up_arrow,
+                        ARROW_WIDTH, ARROW_HEIGHT, 1);
+  }
+  if (scroll_ < wm_helper_.GetNumSettings() + NUM_SPECIAL_SETTINGS - 1) {
+    display_.drawBitmap(display_.width() - ARROW_WIDTH,
+                        display_.height() - ARROW_HEIGHT, down_arrow,
+                        ARROW_WIDTH, ARROW_HEIGHT, 1);
+  }
+
   display_.display();
 }
--- firmware/src/settings.h
+++ firmware/src/settings.h
@@ -7,12 +7,12 @@ class Settings : public App {
  public:
   Settings(Adafruit_SSD1306& display, const WifiManagerParamHelper& wm_helper)
       : display_(display),  wm_helper_(wm_helper) {}
-  void Up() override {};
-  void Down() override {};
+  void Up() override;
+  void Down() override;
   void Left() override {};
   void Right() override {};
-  void Increment() override {};
-  void Decrement() override {};
+  void Increment() override {Up();};
+  void Decrement() override {Down();};
   void Display() override;
 
   const char* GetName() const override {
@@ -20,7 +20,8 @@ class Settings : public App {
   }
 
  private:
-  static constexpr size_t NUM_SPECIAL_SETTINGS = 1;
+  static constexpr size_t NUM_SPECIAL_SETTINGS = 2;
   Adafruit_SSD1306& display_;
   const WifiManagerParamHelper& wm_helper_;
+  size_t scroll_ = 0;
 };
